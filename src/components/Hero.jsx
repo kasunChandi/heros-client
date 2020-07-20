09209ac141948fdbe682ddc8a15ef27728688830
kasun Chandimal <39762853+kasunChandi@users.noreply.github.com>
2020-07-20 20:21:06 +0530

    Initial commit

---
+++ src/components/Hero.jsx
@@ -0,0 +1,13 @@
+
+import React, { Component } from "react";
+
+class Hero extends Component{
+
+    state = {};
+    render(){
+        return <h1>Avengers incoming.....</h1>
+    }
+
+}
+
+export default Hero;
